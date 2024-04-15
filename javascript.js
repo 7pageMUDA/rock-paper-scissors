69d424ea52ede6e4fb1398c1aaa36823727b3163
7pageMUDA <pasricha.nimit@gmail.com>
2024-04-15 17:37:34 -0500

    Add function to play a game

--- javascript.js
+++ javascript.js
@@ -1,10 +1,40 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 1) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    switch (playerSelection.toLowerCase()) {
+        case computerSelection:
+            console.log("It's a draw!");
+            break;
+        case "rock":
+            if (computerSelection === "Paper") {
+                console.log("You Lose! Paper beats Rock");
+            } else {
+                console.log("You Win! Rock beats Scissors");
+            }
+            break;
+        case "paper":
+            if (computerSelection === "Scissors") {
+                console.log("You Lose! Scissors beats Paper");
+            } else {
+                console.log("You Win! Paper beats Rock");
+            }
+            break;
+        case "scissors":
+            if (computerSelection === "Rock") {
+                console.log("You Lose! Rock beats Scissors");
+            } else {
+                console.log("You Win! Scissors beats Paper");
+            }
+            break;
     }
 }
